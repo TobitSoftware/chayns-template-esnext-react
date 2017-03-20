aae9653fdd375995bd89c9e7a82b69bf2c74aded
Kevin Mathmann <kevin.mathmann@gmail.com>
2017-03-20 21:07:00 +0100

    Improve UserList component
    Replace es5 code with es6
    Fix css Block Element Modifier syntax
    Rename renderContent to getUserList for better understanding

--- src/components/user_view/UserList.jsx
+++ src/components/user_view/UserList.jsx
@@ -4,48 +4,47 @@ import Accordion from 'tobit-chayns_components/react-chayns-accordion';
 import './UserList.scss';
 
 export default class UserList extends React.Component {
-
-    //whenever the user prop gets a new value re-render this element. Users were added in the Content.jsx via the PersonFinder
+    /**
+     * Whenever the user prop gets a new value re-render this element. Users were added in the Content.jsx via the PersonFinder
+     */
     static propTypes = {
         user: React.PropTypes.arrayOf(React.PropTypes.object)
     };
 
-    constructor() {
-        super();
-    }
-
     /**
-     * If at least one user was selected, render the users inside the user list
-     * Else return a placeholder text
-     *
+     * If at least one user was selected, render the users inside the user list,
+     * else return a placeholder text.
      */
-    renderContent(){
-        if(this.props.user.length > 0) {
-            return this.props.user.map((user, index) => {
+    getUserList() {
+        const { user } = this.props;
+        if (user.length > 0) {
+            return user.map(({ name, facebookId }, index) => {
                 return (
                     <div className="user" key={index}>
                         <div
-                            className="user_image"
-                            style={{backgroundSize: 'cover', backgroundImage: 'url(http://graph.facebook.com/' + user.facebookId + '/picture?type=square'}}
-                        >
-                        </div>
-                        <div className="user_name">
-                            {user.name}
+                            className="user__image"
+                            style={{
+                                backgroundSize: 'cover',
+                                backgroundImage: `url(http://graph.facebook.com/${facebookId}/picture?type=square`
+                            }}
+                        ></div>
+                        <div className="user__name">
+                            {name}
                         </div>
                     </div>
                 )
             })
         } else {
-            return <div>No users have been selected</div>
+            return <div>No users have been selected.</div>
         }
     }
 
     render() {
-        return(
+        return (
             <Accordion head="User List" defaultOpened>
                 <div className="accordion__content" id="usersList">
                     {
-                        this.renderContent()
+                        this.getUserList()
                     }
                 </div>
             </Accordion>
