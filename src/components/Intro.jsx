715840e4f960cc71d7a56a9fa7f7f423b096d57b
Kevin Mathmann <kevin.mathmann@gmail.com>
2017-03-21 09:10:08 +0100

    Improve Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 
-const HEADLINE = "chayns user selector",
-    DESCRIPTION = "This tapp lets chayns® Managers switch to an 'admin mode' which displays secured content and enables to select users via the chayns® person finder. " +
-        "Selected users will be added to a list that can be seen in both admin and user modes." +
-        "This is an example of a ModeSwitch implementation and shows you how to secure content by UAC groups.";
+import { HEADLINE, DESCRIPTION } from '../constants/text';
 
-const Intro = () => {
-    return (
-        <div className="tapp__intro">
-            <h1 className="headline">
-                {HEADLINE}
-            </h1>
-            <p>
-                {DESCRIPTION}
-            </p>
-        </div>
-    );
-};
+const Intro = () => (
+    <div className="tapp__intro">
+        <h1 className="headline">
+            {HEADLINE}
+        </h1>
+        <p>
+            {DESCRIPTION}
+        </p>
+    </div>
+);
 
 export default Intro;
